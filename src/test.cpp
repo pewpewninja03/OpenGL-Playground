4658053d56c255be4cb5b3ae3553b7bc789f9ad4
pewpewninja03 <tommyduong03@gmail.com>
2026-03-23 01:19:28 -0700

    Draw 2 triangles based off EBO

--- src/test.cpp
+++ src/test.cpp
@@ -51,13 +51,6 @@ int main() {
 
   framebuffer_size_callback(window, width, height);
 
-  // create buffer of vertices
-  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
-  unsigned int VBO;
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
   // Compile shader
   unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -75,35 +68,59 @@ int main() {
   // compile fragment shader
   unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  // int success;
-  // char infoLog[512];
-  // glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-  // if (!success) {
-  //   glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-  //   std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
-  //             << infoLog << std::endl;
-  //   return -1;
-  // }
+  glCompileShader(fragmentShader);
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+    return -1;
+  }
 
   // attach all shaders together
   unsigned int shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
   glLinkProgram(shaderProgram);
-  int shaderProgramSuccess;
-  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &shaderProgramSuccess);
-  if (!shaderProgramSuccess) {
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
     glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
     std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n"
               << infoLog << std::endl;
     return -1;
   }
+
   // No longer needed after linked successfully
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
-  glUseProgram(shaderProgram);
+  float vertices[] = {
+      0.5f,   0.5f,  0.0f, // top right 0
+      0.5f,   -0.5f, 0.0f, // bottom right 1
+      -0.5f,  -0.5f, 0.0f, // bottom left 2
+      -0.5f,  0.5f,  0.0f, // top left 3
+      0.25f,  0.5f,  0.0f, // top right middle 4
+      -0.25f, 0.5f,  0.0f, // top left middle 5
+      0.0f,   -0.5f, 0.0f, // bottom 6
+  };
+  unsigned int indices[] = {
+      2, 5, 6, // first triangle (top right, bottom right, top left)
+      1, 4, 6  // second triangle (bottom right, bottom left, top left)
+  };
+  unsigned int VAO, VBO,
+      EBO; // vao = vertex array, vbo = vertices , ebo = indices
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  // Set up vertix attributes
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
@@ -111,8 +128,18 @@ int main() {
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
 
+    // Background color
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // Tell opengl to render our object
+    glUseProgram(shaderProgram);
+
+    // Draw triangle
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
